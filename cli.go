2b24bdc09c65d04169cba4c3f4659b4f284c5151
Adnan Issadeen <eddyrox1@hotmail.com>
2018-03-26 10:26:23 +0530

    Updated error and help messaging

--- cli.go
+++ cli.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"fmt"
 	"log"
 
 	"github.com/urfave/cli"
@@ -10,25 +11,31 @@ import (
 
 func main(){
 	app := cli.NewApp()
+	app.Name = "Slack Status Updater"
+	app.Usage = "Painless status updating via your Terminal"
+	app.Version = "0.0.1 alpha super pre release"
 	slackToken := os.Getenv("SLACKPERSONALTOKEN")
 	fileToReadStatusesFrom := os.Getenv("HOME") + "/.config/ssucli/statuses.yaml"
 	app.Action = func(c *cli.Context) error {
 		statusName := c.Args().Get(0)
 		statusList, err := getStatusesFromFile(fileToReadStatusesFrom)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Printf("There was an error while trying to read the status list: %s\n", err.Error())
+			return nil
 		}
 		statusToSet, err := pickStatusFromList(statusName, statusList)
 		if err != nil {
-			log.Fatal(err)
+			fmt.Printf("There was an error while trying to find the status you named: %s\n", err.Error())
+			return nil
 		}
 		res, err := statusToSet.SetMyStatus(ssucore.UpdateStatusViaSDK, slackToken)
 		if res != "" {
-			log.Printf(res)
+			fmt.Printf("Status post result: %s\n", res)
 		}
 
 		if err != nil {
-			log.Fatal(err)
+			fmt.Printf("There was an error while setting the status %s\n", err.Error())
+			return nil
 		}
 		return nil
 	}
